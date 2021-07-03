7f1c700aea82b6d0cf27399c360e24ab460f0c32
Batuhan Erden <erden.batuhan@gmail.com>
2021-07-03 02:11:30 +0200

    add sequential improvements & fix the bug in omp implementation

--- gcn_sequential_fast.cpp
+++ gcn_sequential_fast.cpp
@@ -1,26 +1,22 @@
 /*
  *
- * File: gcn_omp.cpp
+ * File: gcn_sequential_fast.cpp
  * Author: Batuhan Erden
  * Created by: Batuhan Erden
  * Created on: Jun 9, 2021
  *
  */
 
-#include <omp.h>
-
 #include "Model.hpp"
 #include "Node.hpp"
 
 
 #define DEBUG 0
-#define NUM_THREADS 16
 
 
 /***************************************************************************************/
 void create_graph(Node** nodes, Model& model) {
     // set neighbor relations
-    #pragma omp parallel for
     for (int e = 0; e < model.num_edges; ++e) {
         int source = model.edges[e];
         int target = model.edges[model.num_edges + e];
@@ -33,7 +29,6 @@ void create_graph(Node** nodes, Model& model) {
     }
 
     // add self-loops
-    #pragma omp parallel for
     for (int n = 0; n < model.num_nodes; ++n) {
         Node *node = nodes[n];
 
@@ -48,27 +43,17 @@ void create_graph(Node** nodes, Model& model) {
 void first_layer_transform(Node** nodes, int num_nodes, Model& model) {
     // transform
     for (int n = 0; n < num_nodes; ++n) {
-        #pragma omp task default(none) firstprivate(nodes, n, model)
-        {
-            Node* node = nodes[n];
+        Node* node = nodes[n];
 
-            for (int c_in = 0; c_in < node->dim_features; ++c_in) {
-                float x_in = node->x[c_in];
-                float* weight_1_start_idx = model.weight_1 + (c_in * node->dim_hidden);
+        for (int c_in = 0; c_in < node->dim_features; ++c_in) {
+            float x_in = node->x[c_in];
+            float* weight_1_start_idx = model.weight_1 + (c_in * node->dim_hidden);
 
-	            // if the input is zero, do not calculate the corresponding hidden values
-	            if (x_in == 0) {
-	                continue;
-	            }
-
-                for (int c_out = 0; c_out < node->dim_hidden; ++c_out) {
-                    node->tmp_hidden[c_out] += x_in * *(weight_1_start_idx + c_out);
-                }
+            for (int c_out = 0; c_out < node->dim_hidden; ++c_out) {
+                node->tmp_hidden[c_out] += x_in * *(weight_1_start_idx + c_out);
             }
         }
     }
-
-    #pragma omp taskwait
 }
 /***************************************************************************************/
 
@@ -76,7 +61,6 @@ void first_layer_transform(Node** nodes, int num_nodes, Model& model) {
 /***************************************************************************************/
 void first_layer_aggregate(Node** nodes, int num_nodes, Model& model) {
     // aggregate for each node
-    #pragma omp parallel for
     for (int n = 0; n < num_nodes; ++n) {
         Node* node = nodes[n];
 
@@ -106,18 +90,12 @@ void first_layer_aggregate(Node** nodes, int num_nodes, Model& model) {
 // computation in second layer
 void second_layer_transform(Node** nodes, int num_nodes, Model& model) {
     // transform
-    #pragma omp parallel for
     for (int n = 0; n < num_nodes; ++n) {
         Node* node = nodes[n];
 
         for (int c_in = 0; c_in < node->dim_hidden; ++c_in) {
             float h_in = node->hidden[c_in];
             float* weight_2_start_idx = model.weight_2 + (c_in * node->num_classes);
-
-            // if the input is zero, do not calculate the corresponding logits
-            if (h_in == 0) {
-                continue;
-            }
 
             for (int c_out = 0; c_out < node->num_classes; ++c_out) {
                 node->tmp_logits[c_out] += h_in * *(weight_2_start_idx + c_out);
@@ -131,7 +109,6 @@ void second_layer_transform(Node** nodes, int num_nodes, Model& model) {
 /***************************************************************************************/
 void second_layer_aggregate(Node** nodes, int num_nodes, Model& model) {
     // aggregate for each node
-    #pragma omp parallel for
     for (int n = 0; n < num_nodes; ++n) {
         Node* node = nodes[n];
 
@@ -178,10 +155,7 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    omp_set_num_threads(NUM_THREADS);
-
     // initialize nodes
-    #pragma omp parallel for
     for (int n = 0; n < model.num_nodes; ++n) {
         nodes[n] = new Node(n, model, 1);
     }
@@ -189,12 +163,7 @@ int main(int argc, char** argv) {
     create_graph(nodes, model);
 
     // perform actual computation in network
-    #pragma omp parallel
-    #pragma omp single
-    {
-        first_layer_transform(nodes, model.num_nodes, model);
-    }
-
+    first_layer_transform(nodes, model.num_nodes, model);
     first_layer_aggregate(nodes, model.num_nodes, model);
     second_layer_transform(nodes, model.num_nodes, model);
     second_layer_aggregate(nodes, model.num_nodes, model);
@@ -202,7 +171,6 @@ int main(int argc, char** argv) {
     // compute accuracy
     float acc = 0.0;
 
-    #pragma omp parallel for reduction(+: acc)
     for (int n = 0; n < model.num_nodes; ++n) {
         int pred = nodes[n]->get_prediction();
         int correct = pred == model.labels[n];
@@ -224,7 +192,6 @@ int main(int argc, char** argv) {
     #endif
 
     // clean-up
-    #pragma omp parallel for
     for (int n = 0; n < model.num_nodes; ++n) {
         nodes[n]->free_node();
         delete nodes[n];
